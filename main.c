4988ab01270ade7c8724255ae175f7546c3f95a5
AlfaNaufal <alfanaufal79@gmail.com>
2024-12-03 09:19:49 +0700

    hapus second

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 int main(){
 
     time_t second;
-   seconds = time(NULL);
-   printf("Hours since January 1, 1970 = %ld\n", seconds/3600);
+    seconds = time(NULL);
+    printf("Hours since January 1, 1970 = %ld\n", seconds/3600);
     
+            
     return 0;
 }
